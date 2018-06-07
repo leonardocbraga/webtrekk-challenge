55225d0ab20bf6c0dab2060647b10179a3c8fe1d
Leonardo Braga <leonardocbraga90@gmail.com>
2018-06-07 20:22:20 -0300

    updates to deploy to heroku

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,16 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { IndexComponent } from './costumer/index/index.component';
+import { CreateComponent } from './costumer/create/create.component';
+import { EditComponent } from './costumer/edit/edit.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    IndexComponent,
+    CreateComponent,
+    EditComponent
   ],
   imports: [
     BrowserModule
