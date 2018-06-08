16438b6ff9d7933fdc5bb51b06ed1db5a44a9944
Leonardo Braga <leonardocbraga90@gmail.com>
2018-06-07 23:55:44 -0300

    fixing paths

--- server.js
+++ server.js
@@ -18,6 +18,11 @@ const app = express();
 const port = process.env.PORT || 8080;
 
 app.use('/costumers', costumerRoutes);
+app.use(express.static(__dirname + '/dist'));
+
+app.get('/*', function(req,res) {  
+  res.sendFile(path.join(__dirname+'/dist/index.html'));
+});
 
 const server = app.listen(port, function(){
   console.log('Listening on port ' + port);
